ac0d72e5860a7b7e21a2a64a12a90ba8b1acdc1d
TheLeader-Community <TheLeaderOfCommunity@gmail.com>
2025-04-28 12:37:41 +0100

    update bottomBar for mobile

--- app/components/ui/BottomBar.tsx
+++ app/components/ui/BottomBar.tsx
@@ -1,18 +1,19 @@
 import { Card } from "@/components/ui/card";
 import { Cog, Home, User2 } from "lucide-react";
+import Link from "next/link";
 
 export default function BottomBar() {
     return (
         <Card className=" md:hidden bg-background fixed rounded-none bottom-0  left-0 w-full p-3" >
             <div className=" flex justify-around items-center">
-                <div className=" flex flex-col items-center">
+                <Link href={"/portfolio"} className=" flex flex-col items-center">
                     <User2/>
                     <p className=" text-xs">Profile</p>
-                </div>
-                <div className=" flex flex-col items-center">
+                </Link>
+                <Link href={"/"} className=" flex flex-col items-center">
                     <Home/>
                     <p className=" text-xs">Home</p>
-                </div>
+                </Link>
                 <div className=" flex flex-col items-center">
                     <Cog/>
                     <p className=" text-xs">Options</p>
